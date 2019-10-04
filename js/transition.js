65f8967068bb120e0c535555dcfe5ee5ea1db1e8
Rosalind <rosalindjchang@gmail.com>
2019-10-04 15:46:50 -0700

    revert

--- js/transition.js
+++ js/transition.js
@@ -17,11 +17,9 @@ $(document).ready(function(){
 	
 	// page 1 load
 	
-
-	$(window).on('load', function () {
+	if (window.location.pathname == "/index.html" ) {
 		$(".content").removeClass("hidden");
-	});
-	
+      } 
 	
     // from page 1 to page 2
 	
@@ -36,7 +34,8 @@ $(document).ready(function(){
      });
 	
 	
-	if (location.pathname == "/2.html" ) {
+	if (window.location.pathname == "/2.html" ) {
+		$(".img2, p").removeClass("hidden");
 		$(".img2").removeClass("slide2");
         $("happy").mouseover(function(){
             $(".img2").addClass("openeyes");
@@ -57,7 +56,8 @@ $(document).ready(function(){
          },500);
        });
           
-        if (location.pathname == "/3.html" ) {
+        if (window.location.pathname == "/3.html" ) {
+			$(".img3, p").removeClass("hidden");
 			$(".img3").removeClass("lower3");
             $(window).click(function(){
                 var x = event.clientX;
@@ -92,7 +92,7 @@ $(document).ready(function(){
             },1100);
           });
 	
-    	if (location.pathname == "/4.html" ) {
+    	if (window.location.pathname == "/4.html" ) {
 			$("svg, p").removeClass("hidden");
 		} 	
 	
@@ -106,7 +106,7 @@ $(document).ready(function(){
             },1100);
        });
 	
-	if (location.pathname == "/5.html") {
+	if (window.location.pathname == "/5.html") {
 		$("svg").removeClass("hidden").addClass("noaction darkline");
 		$("body").removeClass("dark");
 		$("smile").mouseover(function(){
@@ -128,7 +128,7 @@ $(document).ready(function(){
          },400);
      });
 	
-	if (location.pathname == "/6.html") {
+	if (window.location.pathname == "/6.html") {
 		$("p").removeClass("hidden");
         $(".img6").removeClass("hidden lower6");
 		setTimeout(function() {
@@ -153,7 +153,7 @@ $(document).ready(function(){
 	
 	///////////////////////////////////////////////////////// SAD PAGE
 	
-	if (location.pathname == "/s1.html") {
+	if (window.location.pathname == "/s1.html") {
 		$(".content").removeClass("hidden");
 		setTimeout(function() {
 			$("p").removeClass("down");
